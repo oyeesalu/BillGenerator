b4d703151804a94425f5fa1cdf7a95a973521083
oyeesalu <kamblesupriya943@gmail.com>
2024-08-24 19:06:10 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Pizza basePizza = new Pizza(false);
+        Pizza basePizza = new Pizza(true);
         basePizza.getPizzaPrice();
     }
 }
